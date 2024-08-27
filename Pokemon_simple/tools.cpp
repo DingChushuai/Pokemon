ff784f78a3ddac600012e0d088cc64198d6d9ccb
jy5026@stu.ouc.edu.cn <jy5026@stu.ouc.edu.cn>
2024-08-27 11:07:20 +0800

    1

--- Pokemon_simple/tools.cpp
+++ Pokemon_simple/tools.cpp
@@ -1,9 +1,21 @@
 #include "tools.h"
 
+
 inline vector<string> Split(const string& s, char delimiter)
 {
-    return vector<string>();
+    vector<string>out;
+    string temp = "";
+    for (int i = 0; i < s.size(); i++)
+    {
+        if (s[i] == delimiter) { out.push_back(temp); temp = ""; }
+        else temp += s[i];
+    }
+    out.push_back(temp);
+    return out;
 }
+       
+
+  
 
 inline int stoi(const string& s)
 {
