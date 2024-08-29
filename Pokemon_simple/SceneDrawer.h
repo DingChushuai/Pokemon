01dbe4a5666bc356d50eb8e460487471b1db8139
cpn1317874203@gmail.com <cpn1317874203@gmail.com>
2024-08-29 16:28:50 +0800

    修改了Game.cpp SceneDrawer.cpp  SceneDrawer.h

--- Pokemon_simple/SceneDrawer.h
+++ Pokemon_simple/SceneDrawer.h
@@ -12,15 +12,15 @@ class SceneDrawer
 {
 public:
     SceneDrawer(){};
-    void draw(GameSence gs, int* money = 0, Map* map = nullptr, Log* log = nullptr, Combat* combat = nullptr);    //根据游戏状态绘制场景, 只绘制固定不变的部分, 会随着选择而变化的部分由Command类绘制
+    void draw(GameSence gs, int money = 0, Map* map = nullptr, Log* log = nullptr, Combat* combat = nullptr, Pokemon* pokemon=nullptr);    //根据游戏状态绘制场景, 只绘制固定不变的部分, 会随着选择而变化的部分由Command类绘制
 private:
     void drawMenu();    //只绘制gamebar
     void drawGame(int money, Map* map, Log* log);    //展示人物和地图基本信息和Log, 由于操作会改变地图信息, 所以由Command类绘制
     void drawSetting(); //只显示"设置", 因为设置的具体内容会随着选择而变化, 选中的选择项会高亮, 所以由Command类绘制
     void drawPokemonLib(); //只显示"宝可梦库", 因为宝可梦库的具体内容会随着选择而变化
-    void drawPokemonInfo(); //显示"宝可梦信息"
+    void drawPokemonInfo(Pokemon* pokemon); //显示"宝可梦信息"
     void drawBackpack(); //只显示"背包", 因为背包的具体内容会随着选择而变化
-    void drawPokemonCenter(); //显示"宝可梦中心", 和携带的宝可梦信息
+    void drawPokemonCenter(Pokemon *pokemon); //显示"宝可梦中心", 和携带的宝可梦信息
     void drawShop(int money); //只显示"商店"和钱的信息, 因为商店的具体内容会随着选择而变化
     void drawShooseBuyOrSell(); //只显示"选择购买或出售"
     void drawBuyItem(); //只显示"购买物品"
